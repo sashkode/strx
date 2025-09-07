832266bcbca97767abf16043a9c55bb45d9cb53e
sashkode <alexd@oust.email>
2025-09-06 23:04:59 -0400

    fix(site): suspense the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[--background] text-[--foreground]`}>
+        <Suspense>
         {children}
+        </Suspense>
       </body>
     </html>
   );
